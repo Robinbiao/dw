bb2f0cb90cfcf5842a583c1e173d7f936b0289e4
zhoubiao <zhoubiao@mail.chancein.cn>
2022-07-13 22:21:22 +0800

    隐藏打包的js

--- packages/frame/src/main.js
+++ packages/frame/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
-Vue.use(window.reportModule.default);
+// Vue.use(window.reportModule.default);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
