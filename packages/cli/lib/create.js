23e354803f70cc3d6b635b0947e2658050516c83
zhoubiao <zhoubiao@mail.chancein.cn>
2022-07-25 20:08:08 +0800

    [效能任务单号]T202207206420 [修改内容]添加git初始化 [提交类型]BUG修复 [需要测试]是

--- packages/cli/lib/create.js
+++ packages/cli/lib/create.js
@@ -1,45 +1,59 @@
-const fs = require("fs-extra");
-const path = require("path");
-const inquirer = require("inquirer");
-const { hasGit, hasProjectGit } = require("./util/check-git");
+const fs = require('fs-extra')
+const path = require('path')
+const inquirer = require('inquirer')
+const execRun = require('./util/exec-run')
+const { hasGit, hasProjectGit } = require('./util/check-git')
+
 async function create(projectName, options) {
-  const templateName = options.project ? "project" : "default";
-  const { targetDir, name } = await GenerateAppDir(projectName, options);
-  const copySrc = path.resolve(__dirname, "../template", templateName);
-  fs.copySync(copySrc, targetDir);
-  updatePkg(targetDir, { name });
-  initGit(targetDir);
+  const templateName = options.project ? 'project' : 'default'
+  const { targetDir, name } = await GenerateAppDir(projectName, options)
+  const copySrc = path.resolve(__dirname, '../template', templateName)
+  fs.copySync(copySrc, targetDir)
+  await updatePkg(targetDir, { name })
+  initGit(targetDir)
 }
 
 async function GenerateAppDir(projectName, options) {
-  const cwd = options.cwd || process.cwd();
-  const inCurrent = projectName === ".";
-  const name = inCurrent ? path.relative("../", cwd) : projectName;
-  const targetDir = path.resolve(cwd, projectName || ".");
+  const cwd = options.cwd || process.cwd()
+  const inCurrent = projectName === '.'
+  const name = inCurrent ? path.relative('../', cwd) : projectName
+  const targetDir = path.resolve(cwd, projectName || '.')
   if (fs.existsSync(targetDir)) {
-    await fs.remove(targetDir);
+    await fs.remove(targetDir)
   }
-  fs.mkdirSync(targetDir);
-  return { targetDir, name };
+  fs.mkdirSync(targetDir)
+  return { targetDir, name }
 }
 
 /**
  * 更新package.json信息
  */
 async function updatePkg(targetDir, options = {}) {
-  const pkgSrc = path.resolve(targetDir, "package.json");
-  const pkgPrompt = await inquirer.prompt(require("./util/pkg-prompt"));
+  const pkgSrc = path.resolve(targetDir, 'package.json')
+  const pkgPrompt = await inquirer.prompt(require('./util/pkg-prompt'))
   if (fs.existsSync(pkgSrc)) {
-    const pkg = JSON.parse(fs.readFileSync(pkgSrc, "utf-8"));
-    pkg.name = options.name;
+    const pkg = JSON.parse(fs.readFileSync(pkgSrc, 'utf-8'))
+    pkg.name = options.name
     Object.keys(pkgPrompt).forEach((key) => {
-      pkg[key] = pkgPrompt[key];
-    });
-    fs.writeJsonSync(pkgSrc, pkg, { spaces: 2 });
+      pkg[key] = pkgPrompt[key]
+    })
+    fs.writeJsonSync(pkgSrc, pkg, { spaces: 2 })
   }
 }
-function initGit(cwd) {
+
+/**
+ * 初始化git信息
+ * @param {string} cwd  当前目录地址
+ */
+async function initGit(cwd) {
   if (hasGit()) {
+    // 已经安装git
+    if (!hasProjectGit(cwd)) {
+      await execRun(cwd, 'git init')
+    }
+    await execRun(cwd, 'git add .')
+    await execRun(cwd, 'git commit -m "init"')
   }
 }
-module.exports = create;
+
+module.exports = create
--- packages/cli/lib/util/check-git.js
+++ packages/cli/lib/util/check-git.js
@@ -1,41 +1,42 @@
-const { execSync } = require("child_process");
-const LRU = require("lru-cache"); // 在内存中管理缓存数据
-let _hasGit;
+const { execSync } = require('child_process')
+const LRU = require('lru-cache') // 在内存中管理缓存数据
+let _hasGit
 const _projectGit = new LRU({
   max: 10, // 缓存大小
-  maxAge: 1000, // 缓存过期时间
-});
+  maxAge: 1000 // 缓存过期时间
+})
 
 // 测试是否安装了git
 const hasGit = () => {
   if (_hasGit) {
-    return _hasGit;
+    return _hasGit
   }
   try {
     // 执行shell
-    execSync("git --version", { stdio: "ignore" });
-    return (_hasGit = true);
+    execSync('git --version', { stdio: 'ignore' })
+    return (_hasGit = true)
   } catch (error) {
-    return (_hasGit = false);
+    return (_hasGit = false)
   }
-};
+}
 
 // 测试该项目是否已经是一个git repo
 const hasProjectGit = (cwd) => {
   if (_projectGit.has(cwd)) {
-    return _projectGit.get(cwd);
+    return _projectGit.get(cwd)
   }
-  let result;
+  let result
   try {
-    execSync("git status", { stdio: "ignore" }, cwd);
-    result = true;
+    execSync('git status', { stdio: 'ignore' }, cwd)
+    result = true
   } catch (error) {
-    result = false;
+    result = false
   }
-  _projectGit.set(cwd, result);
-  return result;
-};
+  _projectGit.set(cwd, result)
+  return result
+}
+
 module.exports = {
   hasGit,
-  hasProjectGit,
-};
+  hasProjectGit
+}
